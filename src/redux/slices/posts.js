d239f12114e989f989b59254900aadcdf43fba98
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-08-29 19:55:11 +0500

    +isLoadingPosts?

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from '../../axios';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const { data } = await axios.get('/posts');
+    return data;
+});
 
 const initialState = {
     posts: {
@@ -15,6 +21,20 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducer: {},
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.posts.items = action.payload;
+            state.posts.status = 'loaded';
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'error';
+        },
+    },
 });
 
  export const postsReducer = postsSlice.reducer;
