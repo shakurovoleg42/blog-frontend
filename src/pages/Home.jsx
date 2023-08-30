9c571bb838a258cd9541ae600c643bdbae5f6e76
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-08-30 16:13:16 +0500

    +tags bar, +full post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,19 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts } from '../redux/slices/posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { posts, tags } = useSelector(state => state.posts);
 
   const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
+
 
   React.useEffect(() => {
     dispatch(fetchPosts());
-
+    dispatch(fetchTags());
     // eslint-disable-next-line
   }, []);
   
@@ -36,23 +38,19 @@ export const Home = () => {
               <Post
                 id={obj._id}
                 title={obj.title}
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-                user={{
-                  avatarUrl:
-                    'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                  fullName: 'Keff',
-                }}
-                createdAt={'12 июня 2022 г.'}
-                viewsCount={150}
+                imageUrl="https://www.itransition.com/careers/static/948c186ed9dd77972348647bed88772d/itransition_banner_2_7d4715e112.png"
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
                 commentsCount={3}
-                tags={['react', 'fun', 'typescript']}
+                tags={obj.tags}
                 isEditable
               />
             ),
           )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
