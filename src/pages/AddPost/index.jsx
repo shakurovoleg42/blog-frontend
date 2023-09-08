1e9da0083919920424cc8bb3d15fd4b360107fe5
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-09-08 20:58:21 +0500

    fix "/uploads"

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -30,7 +30,7 @@ export const AddPost = () => {
       const formData = new FormData();
       const file = event.target.files[0];
       formData.append('image', file);
-      const { data } = await axios.post('/upload', formData);
+      const { data } = await axios.post('/uploads', formData);
       setImageUrl(data.url);
     } catch (err) {
       console.warn(err);
