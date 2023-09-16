8b44af9f7daa1878fa3b7cb3d0f019a38a8bb01c
Oleg Shakurov <thorsonodin2003@gmail.com>
2023-09-16 21:11:25 +0500

    .

--- src/components/TagsBlock.jsx
+++ src/components/TagsBlock.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -14,29 +13,34 @@ import { SideBlock } from "./SideBlock";
 export const TagsBlock = ({ items, isLoading = true }) => {
  const { t } = useTranslation();
 
-  return (
-    <SideBlock title={t('tags_block')}>
-      <List>
-        {(isLoading ? [...Array(5)] : items).map((name, i) => (
-          <a
-            style={{ textDecoration: "none", color: "black" }}
-            href={`/tags/${name}`}
-          >
-            <ListItem key={i} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  <TagIcon />
-                </ListItemIcon>
-                {isLoading ? (
-                  <Skeleton width={100} />
-                ) : (
-                  <ListItemText primary={name} />
-                )}
-              </ListItemButton>
-            </ListItem>
-          </a>
-        ))}
+ return (
+   <SideBlock title={t('tags_block')}>
+     <List>
+      {(isLoading ? [...Array(5)] : items).map((name, i) => (
+        <a
+          style={{ textDecoration: "none", color: "black" }}
+          href={`/tags/${name}`}
+        >
+          <ListItem key={i} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <TagIcon />
+              </ListItemIcon>
+              {isLoading ? (
+                <Skeleton width={100} />
+              ) : (
+                <ListItemText primary={name} />
+              )}
+            </ListItemButton>
+          </ListItem>
+        </a>
+      ))}
       </List>
-    </SideBlock>
-  );
-};
+   </SideBlock>
+ );
+}
+
+
+
+
+
